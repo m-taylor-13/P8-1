a55b538912adf1fd48b9deec16472c010092b409
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-23 13:21:29 -0400

    Completed countVowel function

--- Project21/FileName.cpp
+++ Project21/FileName.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
-int countCharacter(string str);
+int countVowel(string str);
 
 int main(void) {
-	string s = "dog";
-	int length = countCharacter(s);
-	cout << "String " << s << " is " << length << " characters long.";
+	string s = "aeiou";
+	int numVowels = countVowel(s);
+	cout << "String " << s << " has " << numVowels << " vowels";
 }
 
-int countCharacter(string str) {
-	return str.length();
+int countVowel(string str) {
+	int numVowels = 0;
+
+	//parses through the string to see if each character is a vowel
+	for (int i = 0; i < str.length(); i++) {
+		if (tolower(str[i]) == 'a' || tolower(str[i]) == 'e' || tolower(str[i]) == 'i' || tolower(str[i]) == 'o' || tolower(str[i]) == 'u') {
+			numVowels++; //if yes, adds 1 to numVowels
+		}
+	}
+
+	return numVowels;
 }
