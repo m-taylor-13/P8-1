a15798dddc7d8afe622c0777828d710c91bdee07
m-taylor.13 <m-taylor.13@onu.edu>
2025-10-23 13:31:09 -0400

    Completed P8-1

--- Project21/FileName.cpp
+++ Project21/FileName.cpp
@@ -7,15 +7,23 @@ using namespace std;
 int countVowel(string str);
 
 int main(void) {
-	string s = "aeiou";
-	int numVowels = countVowel(s);
-	cout << "String " << s << " has " << numVowels << " vowels";
+	string input;
+	while (true) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q" || input == "q") break;
+
+		else {
+			cout << "Vowel count: " << countVowel(input) << endl;
+		}
+	}
+	return 0;
 }
 
 int countVowel(string str) {
 	int numVowels = 0;
 
-	//parses through the string to see if each character is a vowel
+		//parses through the string to see if each character is a vowel
 	for (int i = 0; i < str.length(); i++) {
 		if (tolower(str[i]) == 'a' || tolower(str[i]) == 'e' || tolower(str[i]) == 'i' || tolower(str[i]) == 'o' || tolower(str[i]) == 'u') {
 			numVowels++; //if yes, adds 1 to numVowels
